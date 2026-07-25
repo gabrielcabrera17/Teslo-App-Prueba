59670830487f9fb9ee67a77de56da38911c608bd
Gabriel <gabrielbaez023@gmail.com>
2026-07-25 10:30:15 -0300

    Go_Router - Protección de Rutas

--- lib/features/auth/presentation/screens/check_auth_status_screen.dart
+++ lib/features/auth/presentation/screens/check_auth_status_screen.dart
@@ -1,18 +1,13 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
-import 'package:teslo_shop/features/auth/presentation/providers/auth_provider.dart';
 
-class CheckAuthStatusScreen extends ConsumerWidget {
+
+class CheckAuthStatusScreen extends StatelessWidget {
   const CheckAuthStatusScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
 
-    ref.listen(authProvider, (previous, next) {
-      context.go('/');
-    },);
     return Scaffold(
       body: Center(
         child: CircularProgressIndicator(strokeWidth: 2),
