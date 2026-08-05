eb7d0c720b981bb924bcff5a1f549778258d8925
Gabriel <gabrielbaez023@gmail.com>
2026-08-05 20:03:03 -0300

    GoRouterNotifier

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,12 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:teslo_shop/config/router/app_router_notifier.dart';
 import 'package:teslo_shop/features/auth/auth.dart';
 import 'package:teslo_shop/features/products/products.dart';
 
 
 final goRouterProvider = Provider((ref) {
+
+  final goRouterNotifier = ref.read(GoRouterNotifierProvider);
+
   return GoRouter(
-  initialLocation: '/splash',
+  initialLocation: '/login',
+  refreshListenable: goRouterNotifier,
     routes: [
 
       ///* Primera Pantalla
@@ -34,6 +39,7 @@ final goRouterProvider = Provider((ref) {
 
     redirect: (context, state) {
       
+      print(state.fullPath);
 
       return null;
     },
