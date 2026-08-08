9f496001ced53f29dfd4362d0232f0be66784c9e
Gabriel <gabrielbaez023@gmail.com>
2026-08-08 09:09:47 -0300

    Navegar dependiendo de la autenticación

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:teslo_shop/config/router/app_router_notifier.dart';
 import 'package:teslo_shop/features/auth/auth.dart';
+import 'package:teslo_shop/features/auth/presentation/providers/auth_provider.dart';
 import 'package:teslo_shop/features/products/products.dart';
 
 
@@ -10,7 +11,7 @@ final goRouterProvider = Provider((ref) {
   final goRouterNotifier = ref.read(GoRouterNotifierProvider);
 
   return GoRouter(
-  initialLocation: '/login',
+  initialLocation: '/splash',
   refreshListenable: goRouterNotifier,
     routes: [
 
@@ -39,7 +40,23 @@ final goRouterProvider = Provider((ref) {
 
     redirect: (context, state) {
       
-      print(state.fullPath);
+      final isGoingTo = state.fullPath;
+      final authStatus = goRouterNotifier.authStatus;
+
+      if( isGoingTo == '/splash' && authStatus == AuthStatus.checking) return null;
+
+
+      if(authStatus == AuthStatus.notAuthenticated){
+
+        if( isGoingTo == '/login' || isGoingTo == '/register') return null;
+
+        return '/login';
+
+      }
+
+      if(authStatus == AuthStatus.authenticated){
+        if( isGoingTo == '/login' || isGoingTo == '/register' || isGoingTo == '/splash') return '/';
+      }
 
       return null;
     },
