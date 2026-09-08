1c23b7d1253768bf19baff4a58abc745898c4416
Gabriel <gabrielbaez023@gmail.com>
2026-09-08 19:28:54 -0300

    Pantalla de Producto

--- lib/features/products/presentation/screens/product_screen.dart
+++ lib/features/products/presentation/screens/product_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ProductScreen extends StatelessWidget {
-  const ProductScreen({super.key});
+class ProductScreen extends ConsumerStatefulWidget {
+
+  final String productId;
+
+  const ProductScreen({super.key, required this.productId});
 
   @override
+  ProductScreenState createState() => ProductScreenState();
+}
+
+class ProductScreenState extends ConsumerState<ProductScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Editar Producto'),
+      ),
+      body: Center(
+        child: Text( widget.productId ),
+      ),
+    );
   }
 }
