0fb58f1b4802519f7c0309989a052ab963209755
Gabriel <gabrielbaez023@gmail.com>
2026-09-21 19:01:16 -0300

    Conectar el provider con el formulario

--- lib/features/products/presentation/screens/product_screen.dart
+++ lib/features/products/presentation/screens/product_screen.dart
@@ -38,14 +38,16 @@ class ProductScreen extends ConsumerWidget {
 }
 
 
-class _ProductView extends StatelessWidget {
+class _ProductView extends ConsumerWidget {
 
   final Product product;
 
   const _ProductView({required this.product});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final productForm = ref.watch(productFromProvider(product));
 
     final textStyles = Theme.of(context).textTheme;
 
@@ -55,11 +57,11 @@ class _ProductView extends StatelessWidget {
           SizedBox(
             height: 250,
             width: 600,
-            child: _ImageGallery(images: product.images ),
+            child: _ImageGallery(images: productForm.images ),
           ),
     
           const SizedBox( height: 10 ),
-          Center(child: Text( product.title, style: textStyles.titleSmall )),
+          Center(child: Text( productForm.title.value, style: textStyles.titleSmall )),
           const SizedBox( height: 10 ),
           _ProductInformation( product: product ),
           
@@ -76,6 +78,8 @@ class _ProductInformation extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref ) {
 
+
+    final productForm = ref.watch(productFromProvider(product));
     
 
     return Padding(
@@ -88,17 +92,23 @@ class _ProductInformation extends ConsumerWidget {
           CustomProductField( 
             isTopField: true,
             label: 'Nombre',
-            initialValue: product.title,
+            initialValue: productForm.title.value,
+            onChanged: ref.read(productFromProvider(product).notifier).onTitleChanged,
+            errorMessage: productForm.title.errorMessage,
           ),
           CustomProductField( 
             label: 'Slug',
-            initialValue: product.slug,
+            initialValue: productForm.slug.value,
+            onChanged: ref.read(productFromProvider(product).notifier).onSlugChanged,
+            errorMessage: productForm.slug.errorMessage,
           ),
           CustomProductField( 
             isBottomField: true,
             label: 'Precio',
             keyboardType: const TextInputType.numberWithOptions(decimal: true),
-            initialValue: product.price.toString(),
+            initialValue: productForm.price.value.toString(),
+            onChanged: (value) => ref.read(productFromProvider(product).notifier).onPriceChanged(double.tryParse(value) ?? 0),
+            errorMessage: productForm.price.errorMessage,
           ),
 
           const SizedBox(height: 15 ),
@@ -114,7 +124,9 @@ class _ProductInformation extends ConsumerWidget {
             isTopField: true,
             label: 'Existencias',
             keyboardType: const TextInputType.numberWithOptions(decimal: true),
-            initialValue: product.stock.toString(),
+            initialValue: productForm.inStock.value.toString(),
+            onChanged: (value) => ref.read(productFromProvider(product).notifier).onStockChanged(int.tryParse(value) ?? 0),
+            errorMessage: productForm.inStock.errorMessage,
           ),
 
           CustomProductField( 
